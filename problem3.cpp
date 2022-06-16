e12c8cae8e82ded1ea7a477ab1626de3f9934dcf
Mahtab-Hossain <alokmahtab88@gmail.com>
2022-06-17 00:27:23 +0600

    modified problem 3 file

---
+++ problem3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int val;
+    val = 0;
+
+    cin>>val;
+    if (val > 2)
+    {
+        cout<<v;
+    
+    }
+    else{
+        cout<<"wrong input"<<endl;
+    }
+    return 0;
+
+    
+}
