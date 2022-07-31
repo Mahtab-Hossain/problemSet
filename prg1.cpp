7a2c8373220a4c07f4ac09ec80a46266eabfd190
Mahtab-Hossain <alokmahtab88@gmail.com>
2022-08-01 00:29:39 +0600

    new files modified

---
+++ prg1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x,y;
+    cout<<"enter the value of x & y :"<<endl;
+    cin>>x>>y;
+    if(x>y){
+        cout<<"the number you inserted is greater then x ";
+        
+    }
+    else{
+        cout<<"the number you inserted is less then x";
+    }
+    return 0;
+}
