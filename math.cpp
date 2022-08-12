0c96883d9737fce3117868106ea29d13339e2695
kali <alokmahtab88@gmail.com>
2022-08-13 02:10:17 +0600

    math exercise

---
+++ math.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int number;
+    cin>>number;
+    cout<<sqrt(number)<<endl;
+    cout<<log(number)<<endl;
+    cout<<expm1(number)<<endl;
+    cout<<floor(number)<<endl;
+    cout<<sinh(number)<<endl;
+    return 0;
+
+}
