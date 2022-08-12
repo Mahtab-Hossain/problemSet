2ce43c0e6372dd924bd74b315a675a97b79ab79e
kali <alokmahtab88@gmail.com>
2022-08-13 01:50:03 +0600

    modified nextround.cpp

--- codeforces/next_round.cpp
+++ codeforces/next_round.cpp
@@ -4,5 +4,20 @@ using namespace std;
 int main(){
     int n,k;
     cin >>n>>k;
-
+    int a;
+    for(int i=0;i<k;++i)
+    {
+        if(cin>>a && a==0){
+            cout<<i<<endl;
+        
+        }
+    }
+    int b;
+    for(int i=k;i<n;++i){
+        if(cin>>b && b != a){
+            cout<<i<<endl;
+        }
+    }
+    cout<<n<<endl;
+    return 0;
 }
