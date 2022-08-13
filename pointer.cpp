05ae8db7592ad089f0c4545c0d0dfacc0d15dc12
kali <alokmahtab88@gmail.com>
2022-08-13 23:23:27 +0600

    pointer file modified

---
+++ pointer.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cin>>x;
+    cout<<"this is the value of x: "<<x;
+    cout<<"\nthis is the value of var_x memory address: "<<&x;
+    return 0;
+}
