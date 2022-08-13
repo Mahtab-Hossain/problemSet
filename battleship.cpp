a5a39d38fa3a75b244d3dc66d11f494277b7a2d3
kali <alokmahtab88@gmail.com>
2022-08-13 13:20:37 +0600

    modified battle ship file

---
+++ battleship.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int row,col,hits,numberOfTurns;
+    bool ship[3][3]={
+        {0,1,1},
+        {1,0,1},
+        {0,0,1}
+
+    };
+    while (hits<3)
+    {
+        cout<<"Selecting coordinates:"<<endl;
+        cout<<"choose a row number between 0 and 2";
+        cin>>row;
+        cout<<"choose a column number between 0 and 2";
+        cin>>col;
+        
+    }
+    
+
+
+
+}
