03f890006afc98adcccf1fc17e073d30035899a8
kali <alokmahtab88@gmail.com>
2022-08-13 23:34:14 +0600

    pointer file modified

--- pointer.cpp
+++ pointer.cpp
@@ -3,8 +3,12 @@ using namespace std;
 
 int main(){
     int x;
+    int *p;
     cin>>x;
+    p=&x;
     cout<<"this is the value of x: "<<x;
-    cout<<"\nthis is the value of var_x memory address: "<<&x;
+    cout<<"\n this is the value of var_x memory address: "<<&x;
+    cout<<"\n p var valaue "<<p;
+    cout<<"\n*p "<<*p;
     return 0;
 }
