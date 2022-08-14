3e5fedc43d399dbfae8dbb9d4ea3a19b0861bc3d
kali <alokmahtab88@gmail.com>
2022-08-14 22:16:32 +0600

    modified circle line file

---
+++ codeforces/circle_line.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int dist,n,d,f[100+2]={0},s,t;
+    cin>>n;
+    for(int i=2;i<n+2;++i){
+        cin>>d;
+        f[i]=f[i-1]+d;
+    }
+    cin>>s>>t;
+    if(s>t){
+        swap(s,t);
+    }
+    int dist
+
+}
