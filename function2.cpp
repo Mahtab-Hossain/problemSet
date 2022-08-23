04e92184e0961dcaa641a8d779038233b5a2950c
kali <alokmahtab88@gmail.com>
2022-08-24 04:40:22 +0600

    added addition function to function2.cpp file

---
+++ function2.cpp
@@ -0,0 +1,10 @@
+//function with double parameter
+double addition(double number1, double number2){
+    cin>>number1>>number2;
+    return number1+number2;
+}
+//main function
+int main(){
+    cout<<addition();
+    return 0;
+}
