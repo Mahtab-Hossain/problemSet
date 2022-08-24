630e9c3e53ebc6cbd39cbfe1d7753ad3209dbbe2
kali <alokmahtab88@gmail.com>
2022-08-24 06:03:02 +0600

    modified array function file

--- arr_function.cpp
+++ arr_function.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 using namespace std;
 //declaring array in a function as a parameter
-void arr_function(int number[x]){
-    for(int i = 0; i < x; i++){
-        cout<<number[i]<<"\n";
+void arr_function(int number[]){
+    cin>>var;
+    for(int i = 0; i < var; i++){
+        cin<<number[i]<<"\n";
     }
+    //cout>>number[i];
 }
 int main(){
-    cin>>x;
-    int number[x];
-    arr_function(number);
+    //cin>>x;
+    //int number[];//={1,5,3,2,5};
+    cout<<arr_function(number);
     return 0;
 }
