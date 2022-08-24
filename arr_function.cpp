4d2ec41cee3f75332f9ee0ca95814bf74d390001
kali <alokmahtab88@gmail.com>
2022-08-24 06:06:21 +0600

    modified array function file

--- arr_function.cpp
+++ arr_function.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 using namespace std;
 //declaring array in a function as a parameter
-void arr_function(int number[]){
-    cin>>var;
-    for(int i = 0; i < var; i++){
-        cin<<number[i]<<"\n";
-    }
-    //cout>>number[i];
+void arr_function(int arr[6],int i, int element){
+    cout<<"Enter 5 Array Elements: ";
+    for(i=0; i<5; i++)
+        cin>>arr[i];
+    cout<<"\nEnter Element to Insert: ";
+    cin>>elem;
+    arr[i] = elem;
+    cout<<"\nThe New Array is:\n";
+    for(i=0; i<6; i++)
+        cout<<arr[i]<<"  ";
+    cout<<endl;
+    return 0;
 }
 int main(){
     //cin>>x;
     //int number[];//={1,5,3,2,5};
-    cout<<arr_function(number);
+    arr_function(0,0,0);
     return 0;
 }
