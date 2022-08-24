978dd0a0e247e86dc606c10cc71b2a40a60544db
kali <alokmahtab88@gmail.com>
2022-08-24 06:45:14 +0600

    added recursive function

---
+++ recursion_ex.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+//declaring recursive method
+int addition(int number){
+    cout<<"Keep adding number for addition and to quit press 1:";
+    if(cin>>number && number > 1){
+        return number + addition(number -1);//the main recursion
+    }
+    else{
+        return 0;
+    }
+}
+int main(){
+    int result = addition(5);
+    cout<<result;
+    return 0;
+}
