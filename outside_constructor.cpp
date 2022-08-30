3a3f693b377e34936014ad03a6e34b1f0db35da5
Mahtab-Hossain <alokmahtab88@gmail.com>
2022-08-31 01:52:31 +0600

    added example of outside constructor

---
+++ outside_constructor.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class fruit{
+    public:
+    fruit(int number1,int number2);
+    total_number(int x,int y){
+        
+    }
+};
+fruit::fruit(int number1,int number2){
+    cout<<"enter number 1 :";
+    cin>>number1;
+    cout<<"enter number 2 :";
+    cin>>number2;
+}
+
+int main(){
+    fruit obj(0,0);
+    return 0;
+}
