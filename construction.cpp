475103475abd2a8cd6a0c7bf87343bb383526b09
Mahtab-Hossain <alokmahtab88@gmail.com>
2022-08-31 00:40:04 +0600

    constructor example

---
+++ construction.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class Book{
+    public:
+    Book(int x, int y){
+        cin>>x;
+        cin>>y;
+    }
+
+};
+int main(){
+    Book obj(0,0);
+    return 0;
+}
