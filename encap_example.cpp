fe8f69e0977306cbb90db09e75440d0d0e7f301e
Mahtab-Hossain <alokmahtab88@gmail.com>
2022-08-31 02:57:03 +0600

    encapsulion example modified

--- encap_example.cpp
+++ encap_example.cpp
@@ -3,21 +3,24 @@ using namespace std;
 
 class Techer{
     private:
-    int student;
+      int student;
     public:
-    int numbmer_of_student(int stdn){
+      void numbmer_of_student(int stdn){
         student = stdn;
         cout<<"enter the number of a student: ";
         cin>>stdn;
     }
-    int number_of_class(int stdn){
-        return stdn;
+    int number_of_class(){
+        return student;
     }
 
 };
 int main(){
     Techer obj;
     obj.numbmer_of_student(0);
-    cout<<obj.numbmer_of_student(0);
+    //cout<<obj.numbmer_of_student(0);
+    cout<<"";
+    cout<<obj.number_of_class();
+
     return 0;
 }
