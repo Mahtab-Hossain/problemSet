abae76dc277364cd94144b99ebd74a673857957b
Mahtab-Hossain <alokmahtab88@gmail.com>
2022-08-31 00:04:03 +0600

    completed class and method example

--- class_example.cpp
+++ class_example.cpp
@@ -4,5 +4,13 @@ using namespace std;
 //declare class and class name
 class chair{
     public:
-    void table(string tab);
+    string table(string tab);
 };
+string chair::table(string tab){
+    return tab;
+}
+int main(){
+    chair ch;
+    cout<<ch.table("This is a table");
+    return 0;
+}
