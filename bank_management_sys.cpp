44b02181769d78b5ad5ba24e566c854fbceba851
Mahtab-Hossain <alokmahtab88@gmail.com>
2022-09-07 21:39:54 +0600

    added variable and method to admin class

--- bank_management_sys.cpp
+++ bank_management_sys.cpp
@@ -2,7 +2,20 @@
 #include <fstream>
 
 using namespace std;
+
 class Admin{
+public:
+    int id,age;
+    string name;
+    string userName[20];
+    string passWord[50];
+public:
+    void add_employe(int x){
+
+    }
+    void updateData(){
+
+    }
 
 };
 class Employee{
