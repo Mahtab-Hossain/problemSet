3dd70b82b3ba053472214297944b3c32cb09f87a
Mahtab-Hossain <alokmahtab88@gmail.com>
2022-09-09 23:58:46 +0600

    modified new file

--- sort_arr_list.cpp
+++ sort_arr_list.cpp
@@ -5,16 +5,16 @@ using namespace std;
 
 
 int main(){
-    int size,a;
+    int size;
     cout<<"insert size: ";
     cin>>size;
     int array[size];
-    for(a=0;a<10;++size){
+    for(int a=0;a<size;++a){
         cin>>array[a];
-
+        array[a]=array[size];
     }
-    cout<<"inserted value :"<<array[a];
+    cout<<"inserted value :"<<array[size];
     cout<<"sorted values"<<"\n";
     
-
+    return 0;
 }
