9aba7de3420b9c9742d9471240a15d130cda04a4
Mahtab-Hossain <alokmahtab88@gmail.com>
2022-09-14 22:59:21 +0600

    modified max number file with max number function

---
+++ max_num.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int max(int x, int y){
+    cout<<"enter two numbers: ";
+    cin>>x>>y;
+    cout<<"the bigest number entered is : ";
+    //the below line means that if x is greater then y then it
+    //will return x else y
+    return (x>y)? x:y;
+}
+
+int main(){
+    cout<<max(0,0);
+    return 0;
+}
