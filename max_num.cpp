ea2c7ded6678d18fd0958fa1e95a710b80eb3e34
Mahtab-Hossain <alokmahtab88@gmail.com>
2022-09-14 23:05:19 +0600

    added two more function for double and float data types

--- max_num.cpp
+++ max_num.cpp
@@ -10,6 +10,22 @@ int max(int x, int y){
     //will return x else y
     return (x>y)? x:y;
 }
+float max(float x, float y){
+    cout<<"enter two numbers: ";
+    cin>>x>>y;
+    cout<<"the bigest number entered is : ";
+    //the below line means that if x is greater then y then it
+    //will return x else y
+    return (x>y)? x:y;
+}
+double max(double x, double y){
+    cout<<"enter two numbers: ";
+    cin>>x>>y;
+    cout<<"the bigest number entered is : ";
+    //the below line means that if x is greater then y then it
+    //will return x else y
+    return (x>y)? x:y;
+}
 
 int main(){
     cout<<max(0,0);
