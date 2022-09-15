63cc0ef51409fce1b483609d6438f489c5628626
Mahtab-Hossain <alokmahtab88@gmail.com>
2022-09-16 00:08:52 +0600

    modified matrix file

--- matrix.cpp
+++ matrix.cpp
@@ -2,25 +2,20 @@
 #include <cmath>
 
 using namespace std;
-// void matrix(){
-//     int i,j, number = 0;
-//     for(i=1; i<=5; i++)
-//     {
-//         for(j=1;j<=5; j++){
-//             cin>>number;
-            
-//         }
-//     }
-// }
-
-int main(){
+void matrix(){
     int i,j, number = 0;
     for(i=1; i<=5; i++)
     {
         for(j=1;j<=5; j++){
             cin>>number;
-            
+            if(number==1){
+                cout<<abs(i-3)+abs(j-3)<<endl;
+            }
         }
     }
+}
+
+int main(){
+    matrix();
     return 0;
 }
