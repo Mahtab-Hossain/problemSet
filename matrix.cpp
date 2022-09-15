8a2d809ff6415acf8a28391cc8b2511c800ca284
Mahtab-Hossain <alokmahtab88@gmail.com>
2022-09-16 00:01:12 +0600

    modified matrix  exercise from codeforces

---
+++ matrix.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+// void matrix(){
+//     int i,j, number = 0;
+//     for(i=1; i<=5; i++)
+//     {
+//         for(j=1;j<=5; j++){
+//             cin>>number;
+            
+//         }
+//     }
+// }
+
+int main(){
+    int i,j, number = 0;
+    for(i=1; i<=5; i++)
+    {
+        for(j=1;j<=5; j++){
+            cin>>number;
+            
+        }
+    }
+    return 0;
+}
