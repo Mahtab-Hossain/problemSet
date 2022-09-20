3e4128d58a6d0f71e1bc0025772c2a877f8f6bde
Mahtab-Hossain <alokmahtab88@gmail.com>
2022-09-21 00:19:47 +0600

    added math function

--- math_exr.cpp
+++ math_exr.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
 #include <algorithm>
+#include <string>
+
+using namespace std;
 
 void math(){
-
+    int array[1001],count=0;
+    string s;
+    cin>>s;
+    for(int i=0; i<s.size(); i++)
+    {
+        if(s[i]=='+'){
+            continue;
+        }
+        else{
+            array[count++]=s[i]-'0';
+        }
+    }
+    sort(array,array+count);
+    for(int i = 0; i<count; i++)
+    {
+        cout<<array[i];
+        if(i==count-1){
+            break;
+        }
+        cout<<"+";
+    }
 }
 int main(){
-
+    math();
     return 0;
 }
