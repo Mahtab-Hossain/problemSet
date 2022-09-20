ec66fb717d90a85212fe0751fab39374ccbcf05d
Mahtab-Hossain <alokmahtab88@gmail.com>
2022-09-20 23:49:53 +0600

    added petya function

--- petya.cpp
+++ petya.cpp
@@ -5,7 +5,14 @@
 #include <cstring>
 
 using namespace std;
+void petya(){
+    string string_1, string_2;
+    cin>>string_1>>string_2;
+    transform(string_1.begin(),string_1.end(), string_1.begin(), ::tolower);
+    transform(string_2.begin(),string_2.end(), string_2.begin(), ::tolower);
+    cout<<strcmp(string_1.c_str(),string_2.c_str())<<endl;
+}
 
 int main(){
-
+    petya();
 }
