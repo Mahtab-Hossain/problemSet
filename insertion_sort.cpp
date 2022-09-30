c19f071d310002d8d2484ea10b85e45ecb7dc633
Mahtab-Hossain <alokmahtab88@gmail.com>
2022-09-30 22:46:09 +0600

    added insertion sort class to the main function

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -13,10 +13,27 @@ class Insertion_sort{
         }
         cout<<arr[a];
     }
+    void insetion_sort(int arr[],int size){
+        for(int step=1;step<size;step++){
+            int key = arr[step];
+            int j= step - 1;
+            while(key<arr[j] && j>=0){
+                arr[j+1]=arr[j];
+            }
+            arr[j+1]=key;
+        }
+
+    }
     
 };
 
 int main(){
+    int num[10]={1,4,3,2,5,7,9,8,10};
+    int size = sizeof(num)/ sizeof(num[0]);
+    Insertion_sort insert;
+    insert.insetion_sort(num,size);
 
+    cout<<"sorted array ";
+    insert.print_array(num,size);
     return 0;
 }
