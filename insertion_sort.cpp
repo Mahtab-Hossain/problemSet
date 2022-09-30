ae9e7a57d8583cfc9db697f81ea82b1dd10a07d6
Mahtab-Hossain <alokmahtab88@gmail.com>
2022-09-30 22:37:14 +0600

    added insertion sort class

---
+++ insertion_sort.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Insertion_sort{
+
+    public:
+
+    void print_array(int arr[],int size){
+        int a;
+        cin>>size;
+        for(int a=0;a<size;a++){
+            cin>>arr[a];
+        }
+        cout<<arr[a];
+    }
+    
+};
+
+int main(){
+
+    return 0;
+}
