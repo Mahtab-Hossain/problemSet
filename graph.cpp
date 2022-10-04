eb8250f67933c16f862d22df914edb25deec4e6c
Mahtab-Hossain <alokmahtab88@gmail.com>
2022-10-04 21:54:44 +0600

    added breath first search class

--- graph.cpp
+++ graph.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <list>
 
 using namespace std;
+
+class Breath_First_search{
+
+    public:
+
+    int numberOfVertices;
+    list<int>* add_list;
+    bool* visited;
+    
+    public:
+
+    Breath_First_search(int vertices);
+        void add_edge(int source, int destination);
+        void bfs(int startVertex);
+        
+
+};
+
+
+
 
 int main(){
     return 0;
