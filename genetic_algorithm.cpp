c7a8dd30aca103a35c7dd8fe2de59d9e0d109769
Mahtab-Hossain <alokmahtab88@gmail.com>
2022-11-10 19:44:23 +0600

    added constant and random number function

--- genetic_algorithm.cpp
+++ genetic_algorithm.cpp
@@ -1,6 +1,18 @@
 #include <bits/stdc++.h>
+#define POPULATION 100
 
 using namespace std;
+
+const string GENES = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890, .-;:_!#%&/()=?@${[]}";
+
+const string AIM = "first evaluation";
+//here s = start and e = end
+int random_number(int s, int e){
+    int range,random_n;
+    range=(e-s)+1;
+    random_n=s+(rand()%range);
+    return random_n;
+}
 
 int main(){
 
